b337230dcfb05ed84e21a3381e5fbc5f2e9186ad
Michael McIntosh <michaeldmc58@gmail.com>
2023-05-12 01:53:05 -0700

    Updated code w/ comments

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 function mult(num) {
-
+// answer declaration
     let answer = 1;
-
+// for loop to cycle from 1 - "num"
     for (let i = 1; i <= num; i++){
+// multiply answer in order from 1 - "num"     
         answer *= i;
     }
         return answer;
